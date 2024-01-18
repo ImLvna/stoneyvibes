65382d54f4e43841a7bc461937a9b378479473b5
Luna <imlvnaa@gmail.com>
2024-01-18 16:58:43 -0700

    welcome to sveltekit

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,9 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import type { Components } from '$lib/vrchat/types';
+	import { getContext } from 'svelte';
+
+	const group: Components.Schemas.Group = getContext('group');
+	console.log(group);
+</script>
+
+<div>{group.description}</div>
