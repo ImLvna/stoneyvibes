33cda27668296dcac4df2b3f95476eed50292b3f
Luna <imlvnaa@gmail.com>
2024-01-19 10:14:02 -0700

    Make ambiguous

--- src/routes/+page.svelte
+++
@@ -1,8 +0,0 @@
-<script lang="ts">
-	import type { Components } from '$lib/vrchat/types';
-	import { getContext } from 'svelte';
-
-	const group: Components.Schemas.Group = getContext('group');
-</script>
-
-<div>{group.description}</div>
